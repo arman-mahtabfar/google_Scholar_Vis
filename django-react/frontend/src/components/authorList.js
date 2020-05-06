ef39985dda3ceada28e358c37ddb0cd3e6313e93
arman.mahtabfar <armanm2@illinois.edu>
2020-05-05 21:52:25 -0400

    Visualization and topic search implemented

--- django-react/frontend/src/components/authorList.js
+++ django-react/frontend/src/components/authorList.js
@@ -15,7 +15,7 @@ class AuthorList extends Component {
         InputName = InputName.split(' ').join('+');
         if (InputName.length > 0) {
             try {
-                const res = await fetch('http://127.0.0.1:8000/api/authors/name='+InputName);
+                const res = await fetch('http://127.0.0.1:8000/api/topics/topic='+InputName);
                 const data = await res.json();
                 console.log("api success!");
                 this.setState({
@@ -50,22 +50,22 @@ class AuthorList extends Component {
           <Table dark>
             <thead>
               <tr>
-                <th>Author_Name</th>
-                <th>Author_Affiliaiton</th>
+                <th>Publication Title</th>
+                <th>Publication Year</th>
               </tr>
             </thead>
             <tbody>
               {!this.state.data || this.state.data.length <= 0 ? (
                 <tr>
                   <td colSpan="6" align="center">
-                    <b>No Author Exists :(</b>
+                    <b>Search for a Topic of Your Interest</b>
                   </td>
                 </tr>
               ) : (
                 this.state.data.map(article => (
-                  <tr key={article.name}>
-                    <td>{article.name}</td>
-                    <td>{article.affiliation}</td>
+                  <tr key={article.pub_title}>
+                    <td>{article.pub_title}</td>
+                    <td>{article.pub_year}</td>
                     
                   </tr>
                 ))
